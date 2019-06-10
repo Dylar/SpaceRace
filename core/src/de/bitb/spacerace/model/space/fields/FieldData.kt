b99f8c75fe7f3a4b6d46a626518c88e544dc5ec5
Kolja Bruns <Kolja.bruns@bornholdtlee.de>
2019-06-10 23:22:41 +0200

    bla

--- core/src/de/bitb/spacerace/model/space/fields/FieldData.kt
+++ core/src/de/bitb/spacerace/model/space/fields/FieldData.kt
@@ -1,24 +1,18 @@
 package de.bitb.spacerace.model.space.fields
 
-import de.bitb.spacerace.database.converter.FieldTypeConverter
-import de.bitb.spacerace.database.converter.PlayerColorConverter
-import de.bitb.spacerace.model.enums.FieldType
-import de.bitb.spacerace.model.objecthandling.PositionData
-import de.bitb.spacerace.model.player.PlayerColor
-import io.objectbox.annotation.Convert
 import io.objectbox.annotation.Entity
 import io.objectbox.annotation.Id
-import io.objectbox.relation.ToMany
-import io.objectbox.relation.ToOne
 
 //@Entity
 //data class FieldData(
 //        @Id
 //        var uuid: Long = 0,
-//        @Convert(converter = FieldTypeConverter::class, dbType = String::class)
-//        var fieldType: FieldType = FieldType.UNKNOWN,
-//        @Convert(converter = PlayerColorConverter::class, dbType = String::class)
-//        var owner: PlayerColor = PlayerColor.NONE) {
+//        var name:String = ""
+////        @Convert(converter = FieldTypeConverter::class, dbType = String::class)
+////        var fieldType: FieldType = FieldType.UNKNOWN,
+////        @Convert(converter = PlayerColorConverter::class, dbType = String::class)
+////        var owner: PlayerColor = PlayerColor.NONE
+//) {
 //
 ////    lateinit var position: ToOne<PositionData>
 ////    lateinit var connections: ToMany<FieldData>
