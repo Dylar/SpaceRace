fb32cb1804efde22484c7d67dca14aed0f83a5e3
Kolja Bruns <Kolja.bruns@bornholdtlee.de>
2019-06-12 19:09:03 +0200

    broken

--- core/src/de/bitb/spacerace/injection/modules/DatabaseModule.kt
+++ core/src/de/bitb/spacerace/injection/modules/DatabaseModule.kt
@@ -4,7 +4,7 @@ import dagger.Module
 import dagger.Provides
 import de.bitb.spacerace.database.PlayerDataSource
 import de.bitb.spacerace.database.PlayerRespository
-import de.bitb.spacerace.model.player.MyObjectBox
+import de.bitb.spacerace.model.MyObjectBox
 import de.bitb.spacerace.model.player.PlayerData
 import io.objectbox.Box
 import io.objectbox.BoxStore
