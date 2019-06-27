44139b7b5212653dec86e1b9cd9592e546368906
Kolja Bruns <Kolja.bruns@bornholdtlee.de>
2019-06-27 13:16:53 +0200

    set correct sourcedata

--- desktop/src/de/bitb/spacerace/desktop/DesktopLauncher.java
+++ desktop/src/de/bitb/spacerace/desktop/DesktopLauncher.java
@@ -2,7 +2,6 @@ package de.bitb.spacerace.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import de.bitb.spacerace.core.MainGame;
 
 public class DesktopLauncher {
     public static void main(String[] arg) {
