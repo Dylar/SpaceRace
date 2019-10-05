4211da61a5904197b08e4655caa84f1ecd8d63ab
Dylar <kolja-bruns@gmx.de>
2019-10-05 03:15:16 +0200

    Update IOSLauncher.java

--- ios/src/main/java/de/bitb/spacerace/IOSLauncher.java
+++ ios/src/main/java/de/bitb/spacerace/IOSLauncher.java
@@ -10,7 +10,7 @@ public class IOSLauncher extends IOSApplication.Delegate {
     @Override
     protected IOSApplication createApplication() {
         IOSApplicationConfiguration config = new IOSApplicationConfiguration();
-        return new IOSApplication(new MainGame(), config);
+        return new IOSApplication(new MainGame("iOS", null), config);
     }
 
     public static void main(String[] argv) {
