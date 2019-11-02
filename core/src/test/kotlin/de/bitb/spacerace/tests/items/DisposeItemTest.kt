3231c7055b515bdb99ef349296996e525b6c41bc
Kolja Bruns <Kolja.bruns@bornholdtlee.de>
2019-11-02 22:30:30 +0100

    more test

--- core/src/test/kotlin/de/bitb/spacerace/tests/items/DisposeItemTest.kt
+++ core/src/test/kotlin/de/bitb/spacerace/tests/items/DisposeItemTest.kt
@@ -25,16 +25,16 @@ class DisposeItemTest : ItemsTest() {
         val item = ItemInfo.MineSlowInfo()
         TestEnvironment()
                 .setPlayerItems { listOf(item) }
+                .setGiftFieldItems { listOf(item) }
                 .initGame()
                 .setToMain2Phase()
                 .apply {
                     assertTrue(currentPlayer.positionField.target.disposedItems.isEmpty())
                     assertTrue(currentPlayer.storageItems.filter { it.itemInfo.type == item.type }.size == 2)
                 }
-                .disposeItem(item)
-                .apply {
-                    assertTrue(currentPlayer.positionField.target.disposedItems.any { it.itemInfo.type == item.type })
-                    assertTrue(currentPlayer.storageItems.filter { it.itemInfo.type == item.type }.size == 1)
+                .disposeItem(item) { result ->
+                    result.playerData.positionField.target.disposedItems.any { it.itemInfo.type == item.type } &&
+                            result.playerData.storageItems.filter { it.itemInfo.type == item.type }.size == 1
                 }
                 .endTurn()
                 .apply { assertTrue(currentPlayer.attachedItems.isEmpty()) }
@@ -44,30 +44,8 @@ class DisposeItemTest : ItemsTest() {
 
                     val dbField = getDBField(currentPlayer.positionField.target.uuid)
                     assertTrue(dbField.disposedItems.isEmpty())
-                    val playerfield = currentPlayer.positionField.target
-                    assertTrue(playerfield.disposedItems.isEmpty())
+                    val playerField = currentPlayer.positionField.target
+                    assertTrue(playerField.disposedItems.isEmpty())
                 }
     }
-
-//
-//    @Test
-//    fun activateItem_onRoundEnd_removeIt() {
-//        val item = ItemInfo.EXTRA_FUEL()
-//        TestEnvironment()
-//                .setPlayerItems { listOf(item) }
-//                .initGame()
-//                .apply { assertTrue(currentPlayer.storageItems.isNotEmpty()) }
-//                .activateItem(item) { equipResult ->
-//                    equipResult.playerData.storageItems.isEmpty() &&
-//                            equipResult.playerData.activeItems.find { it.id == equipResult.itemData.id }?.itemInfo?.charges?.let { it > 0 } ?: false
-//                }.setToMovePhase()
-//                .move()
-//                .apply { move(target = circleStep1Field) }
-//                .nextPhase()
-//                .endRound()
-//                .apply {
-//                    assertTrue(currentPlayer.storageItems.isEmpty())
-//                    assertTrue(currentPlayer.activeItems.isEmpty())
-//                }
-//    }
 }
