5314804f40ce4d38dbbda81a91cb5a17c7a71e8e
Kolja Bruns <Kolja.bruns@bornholdtlee.de>
2019-11-28 10:15:45 +0100

    dice btn disabled when rdy

--- core/src/main/kotlin/de/bitb/spacerace/ui/screens/game/control/SRActionGui.kt
+++ core/src/main/kotlin/de/bitb/spacerace/ui/screens/game/control/SRActionGui.kt
@@ -1,8 +1,10 @@
 package de.bitb.spacerace.ui.screens.game.control
 
+import com.kotcrab.vis.ui.widget.VisTextButton
 import de.bitb.spacerace.config.dimensions.Dimensions.GameGuiDimensions.GAME_BUTTON_HEIGHT_DEFAULT
 import de.bitb.spacerace.config.dimensions.Dimensions.GameGuiDimensions.GAME_BUTTON_WIDTH_DEFAULT
 import de.bitb.spacerace.config.dimensions.Dimensions.SCREEN_WIDTH
+import de.bitb.spacerace.config.strings.Strings.GameGuiStrings
 import de.bitb.spacerace.core.MainGame
 import de.bitb.spacerace.core.controller.PlayerController
 import de.bitb.spacerace.core.events.commands.phases.NextPhaseCommand
@@ -10,19 +12,29 @@ import de.bitb.spacerace.core.events.commands.player.DiceCommand
 import de.bitb.spacerace.ui.base.SRAlign
 import de.bitb.spacerace.ui.base.SRGuiGrid
 import de.bitb.spacerace.ui.screens.GuiNavi
+import de.bitb.spacerace.usecase.game.observe.ObservePlayerUseCase
+import io.reactivex.disposables.Disposable
 import org.greenrobot.eventbus.EventBus
 import javax.inject.Inject
 
 class SRActionGui : SRGuiGrid() {
 
     @Inject
+    protected lateinit var observePlayerUseCase: ObservePlayerUseCase
+    private lateinit var disposable: Disposable
+
+    @Inject
     protected lateinit var playerController: PlayerController
+
+    private lateinit var diceBtn: VisTextButton
 
     init {
         MainGame.appComponent.inject(this)
 
         setContent()
         setDimensions()
+
+        initObserver()
     }
 
     private fun setDimensions() {
@@ -36,6 +48,12 @@ class SRActionGui : SRGuiGrid() {
         )
     }
 
+    private fun initObserver() {
+        disposable = observePlayerUseCase.observeStream(playerController.currentColor) { player ->
+            diceBtn.isDisabled = !player.phase.isMain1() || player.hasDicedEnough()
+        }
+    }
+
     private fun setContent() {
         addEmptySlot()
         setStorageBtn()
@@ -45,7 +63,7 @@ class SRActionGui : SRGuiGrid() {
 
     private fun setStorageBtn() {
         addButton(
-                text = "Storage",
+                text = GameGuiStrings.GAME_BUTTON_STORAGE,
                 listener = {
                     val storageMenu: GuiNavi = GuiNavi.StorageMenu(playerController.currentColor)
                     EventBus.getDefault().post(storageMenu)
@@ -54,19 +72,19 @@ class SRActionGui : SRGuiGrid() {
 
     private fun setDiceBtn() {
         addButton(
-                text = "Dice",
+                text = GameGuiStrings.GAME_BUTTON_DICE,
                 listener = { DiceCommand.get(playerController.currentColor).push() })
+                .also { diceBtn = it }
     }
 
     private fun setContinueBtn() {
-        addButton(text = "Continue",
+        addButton(text = GameGuiStrings.GAME_BUTTON_CONTINUE,
                 listener = { EventBus.getDefault().post(NextPhaseCommand.get(playerController.currentColor)) })
     }
 
-    private fun addButton(text: String, listener: () -> Unit) {
-        createTextButtons(
-                text = text,
-                listener = listener)
-                .also { addActor(it) }
-    }
+    private fun addButton(text: String, listener: () -> Unit) =
+            createTextButtons(
+                    text = text,
+                    listener = listener)
+                    .also { addActor(it) }
 }
